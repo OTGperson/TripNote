62d5a82d5dc0f42b36dfe5d0cf06d077e96fec94
OTGperson <dlworud0412@gmail.com>
2025-11-20 19:15:50 +0900

    work

--- backend/src/main/java/com/backend/domain/destination/service/DestinationService.java
+++ backend/src/main/java/com/backend/domain/destination/service/DestinationService.java
@@ -1,9 +1,11 @@
 package com.backend.domain.destination.service;
 
+import com.backend.domain.destination.repository.DestinationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class DestinationService {
+  private final DestinationRepository destinationRepository;
 }
